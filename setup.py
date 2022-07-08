0fdb5f21204152f79c99af54f161162e3904b967
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2022-07-08 14:20:05 -0300

    Archivos aula 5

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Este repositorio contiene los archivos referentes al curso MLOps: Deploy de Modelos de Alura Latam.',
+    author='alvaro.camacho',
+    license='',
+)
